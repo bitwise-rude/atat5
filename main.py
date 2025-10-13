381aa9c54fb2209568548ba76f6abcb3e215ff62
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-10-13 15:13:07 +0545

    can reat file

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+def read_file() -> str:
+    # read file from argument
+    if len(sys.argv) > 1:
+        try:
+            file_ = open(sys.argv[1],'r')
+        except:
+            print_invalid_usage_error("FILE NOT FOUND")
+        contents = file_.read()
+        file_.close()
+        return contents
+    print_invalid_usage_error(f"USE:     {sys.argv[0]} <filename>")
+
+def print_invalid_usage_error(msg:str) -> None:
+    print(f"\n\tINVALID USAGE:\n\t\t{msg}")
+    quit()
+
+contents = read_file()
