22354de6d2f9fee969048cb87683a0eec1fdc40b
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-10-25 16:13:29 +0545

    some work

--- codegen.py
+++ codegen.py
@@ -92,6 +92,11 @@ class CodeGen:
                 _footer = f"\nCMP B\nJC TEMP{self._temp_label_index}\nMVI B,00H\nJMP TEMP{self._temp_label_index+1}\nTEMP{self._temp_label_index}:\nMVI B,01H\nTEMP{self._temp_label_index+1}:\n"
                 self._temp_label_index += 2
             
+            elif node.name == 'LESS_THAN_EQ':
+                _ = self._eval_expression(node.left,reg=reg)
+                _footer = f"\nCMP B\nJC TEMP{self._temp_label_index}\nJZ TEMP{self._temp_label_index}\nMVI B,00H\nJMP TEMP{self._temp_label_index+1}\nTEMP{self._temp_label_index}:\nMVI B,01H\nTEMP{self._temp_label_index+1}:\n"
+                self._temp_label_index += 2
+            
             elif node.name == 'EQUALS_TO':
                 _ = self._eval_expression(node.left,reg=reg)
                 _footer = f"\nCMP B\nJZ TEMP{self._temp_label_index}\nMVI B,00H\nJMP TEMP{self._temp_label_index+1}\nTEMP{self._temp_label_index}:\nMVI B,01H\nTEMP{self._temp_label_index+1}:\n"
